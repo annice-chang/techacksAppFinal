165c6bb75e73290095fcc8dd9df01b3d77fa2ae7
Annice Chang <anniceychang2005@gmail.com>
2021-08-14 17:07:44 -0400

    added view controllers

---
+++ techacksApp/quizOneQuesOneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  quizOneQuesOneViewController.swift
+//  techacksApp
+//
+//  Created by Annice Chang on 8/14/21.
+//
+
+import UIKit
+
+class quizOneQuesOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
